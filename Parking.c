ec93978a4334c38fcfa8400d8422d97be437c944
tanu <24976766+tanusgit@users.noreply.github.com>
2020-06-03 13:16:17 -0700

    updated worksheet

--- Parking.c
+++ Parking.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-int main(void) {
+int main(void)
+{
     printf("  NO PARKING\n");
     printf("2:00 - 6:00 a.m.\n");
     return 0;
---
+++ new.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // Draw tree
+    printf("   *\n");
+    printf("  ***\n");
+    printf(" *****\n");
+    printf("*******\n");
+    printf("  ***\n");
+
+    //printitng cat
+    printf("\n");
+    printf("\n");
+    printf("/\\   /\\\n");
+    printf("  o o\n");
+    printf(" =   =\n");
+    printf("  ---\n");
+
+    return 0;
+}
