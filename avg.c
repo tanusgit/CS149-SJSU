34c73284bf85220d08bc7db7039c23e6920b14a3
tanu <24976766+tanusgit@users.noreply.github.com>
2020-06-05 17:07:32 -0700

    added avg.c

--- avg.c
+++ avg.c
@@ -3,24 +3,21 @@
 
 void average();
 
-int main(int argc, char* argv[])
-{   
-    char* filename = argv[1];
+int main(int argc, char *argv[])
+{
+    char *filename = argv[1];
     average(filename);
 }
 
-void average(char* filename)
+void average(char *filename)
 {
     double sum = 0;
     int count = 0;
     char buffer[1000];
     FILE *fp = fopen(filename, "r");
-
-    //reading all the numbers and checking if reached end of file
+    //reading all the numbers till end of file
     while (fscanf(fp, "%s", buffer) != EOF)
-    {
-        
-        //converting string to int
+    { //converting string to int
         float x = atof(buffer);
         //adding all the numbers
         sum = sum + x;
@@ -30,6 +27,5 @@ void average(char* filename)
     double avg = (sum / count);
     //displaying the average of the numbers
     printf("%lf", avg);
-
     fclose(fp);
 }
