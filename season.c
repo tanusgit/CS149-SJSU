7d1bccedf7467f6957d08341267788d2f693d2e0
tanu <24976766+tanusgit@users.noreply.github.com>
2020-06-07 13:33:46 -0700

    season

--- season.c
+++ season.c
@@ -8,20 +8,91 @@ Winter: December 21 - March 19
 #include <stdio.h>
 #include <string.h>
 
-int main(void) {
-   char Month[50];
-   int Day;
-   
-   scanf("%s", &Month); 
-   scanf("%d", &Day); 
-   //strcmp is used to compare two strings
-  // if (strcmp(Month, "March") == 0) 
-   if (strcmp(Month, "March") == 0) {
-       if(Day >20){
-           printf("Spring");
-       }
+int main(void)
+{
+    char Month[50];
+    int Day;
 
-   }
+    scanf("%s", &Month);
+    scanf("%d", &Day);
+    //strcmp is used to compare two strings
+    // if (strcmp(Month, "March") == 0)
+    if (Day < 1 && Day > 31)
+    {
+        printf("Invalid");
+    }
+    else if ((strcmp(Month, "March") != 0))
+    {
+        printf("Invalid");
+    }
+    else if ((strcmp(Month, "June") != 0))
+    {
+        printf("Invalid");
+    }
+    else if ((strcmp(Month, "September") != 0))
+    {
+        printf("Invalid");
+    }
+    else if ((strcmp(Month, "December") != 0))
+    {
+        printf("Invalid");
+    }
+    else if (strcmp(Month, "March") == 0)
+    {
+        if (Day > 20)
+        {
+            printf("Spring");
+        }
+    }
+    else if (strcmp(Month, "June") == 0)
+    {
+        if (Day < 21)
+        {
+            printf("Spring");
+        }
+    }
+    else if (strcmp(Month, "June") == 0)
+    {
+        if (Day > 20)
+        {
+            printf("Summer");
+        }
+    }
+    else if (strcmp(Month, "September") == 0)
+    {
+        if (Day < 22)
+        {
+            printf("Summer");
+        }
+    }
+    else if (strcmp(Month, "September") == 0)
+    {
+        if (Day > 21)
+        {
+            printf("Autumn");
+        }
+    }
+    else if (strcmp(Month, "December") == 0)
+    {
+        if (Day < 21)
+        {
+            printf("Autumn");
+        }
+    }
+    else if (strcmp(Month, "Deceember") == 0)
+    {
+        if (Day > 20)
+        {
+            printf("Winter");
+        }
+    }
+    else if (strcmp(Month, "March") == 0)
+    {
+        if (Day > 20)
+        {
+            printf("Winter");
+        }
+    }
 
-   return 0;
+    return 0;
 }
