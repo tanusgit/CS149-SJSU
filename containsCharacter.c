3e1cc4b4daf0d9a00c0b985a8cb13cb3828363ea
tanu <24976766+tanusgit@users.noreply.github.com>
2020-06-10 12:55:32 -0700

    below amount

---
+++ containsCharacter.c
@@ -0,0 +1,13 @@
+/*
+Write a program that reads an integer, a list of words, and
+a character. The integer signifies how many words are in
+the list. The output of the program is every word in the
+list that contains the character at least once. Assume at least 
+one word in the list will contain the given character. Assume that 
+the list will always contain less than 20 words. Each word will always
+contain less than 10 characters and no spaces.
+input = 4 hello zoo sleep drizzle z
+output= 
+zoo
+drizzle
+*/
