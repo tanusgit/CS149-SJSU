afe3c29d649162a918683c25c241ae7903a02083
tanu <24976766+tanusgit@users.noreply.github.com>
2020-06-14 17:00:12 -0700

    wait

--- fork.c
+++ fork.c
@@ -3,12 +3,18 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+
+void add(){
+    printf("add = %d\n", 4+5 );
+}
+void sub(){
+    printf("multiplication = %d\n", 4*5 );
+}
 int main(int argc, char *argv[])
 {
     fork();
-    fork();
-    fork();
-    fork();
     fprintf(stderr, "hello\n");
+    add();
+    sub();
     return 0;
 }
