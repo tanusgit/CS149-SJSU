02423c776720cad93b295ff09ce3ccccb48faa70
tanu <24976766+tanusgit@users.noreply.github.com>
2020-07-10 18:55:14 -0700

    stock.c

--- stock.c
+++ stock.c
@@ -7,68 +7,25 @@
 //100 180 260 310 40 535 695
 //output (0 3) (4 6)
 #include <stdio.h>
-
-int main()
-{
-    
-    int days;
-    scanf("%d", &days);
-
-
-    int array[days];
-    for(int i = 0; i< days; i++){
-        scanf("%d", &array[i]);
-    }
-   
-    int profit;
-    int indexmin;
-    int indexmax;
-    for(int j =0; j < days; j++){
-    for(int i = 0; i< days; i++){
-         int min= array[i];
-        int max = array[i+1];
-        indexmin = i;
-        if(min < max){
-          i++;
-        }
-        else{
-          indexmax = i-1;
-          profit = array[i-1]-min;
-          min = array[i];
+#include <stdio.h>
+int main() {
+    int ar[6]={7,1,5,3,6,4};
+    int max = 0; 
+    int profit = 0;
+    for(int i = 0; i < 5; i++){
+        //looking at the back and start at the 1st index and compare those
+        //2 elements and if the previous element is lower than the current
+        //then subtract and save and it to profit
+        int max = ar[i+1];
+        if(max > ar[i]){
+            profit =  profit + ar[i+1] - ar[i];
+              
         }
     }
-    }
-    printf("profit = %d\n", profit);
-     printf("days = %d %d\n", indexmin, indexmax);
-
-
-
-
-
-    /*
-    int start = array[0];
-    int max = array[0];
-    int maxposition = 0;
-    for(int i = 0; i < days; i++){
-        //getting the max value in the array
-        if(array[i] > max){
-            max = array[i];
-            maxposition = i;
-        }
-    }
-    //got the max value poition in the array
-    //now check the max profit == max value - all the values
-    int maxprofit = max - start;
-    int maxprofitpos = 0;
-    for(int i = 0; i < days; i++){
-        if(max- array[i] > maxprofit){
-            maxprofit = max- array[i];
-            maxprofitpos = i;
-        }
-    }
-    //got max profit and its position
-
-    printf("maximum profit = %d, and days include %d", maxprofit, maxprofitpos);*/
-
+    printf("profit %d ", profit);
     return 0;
 }
+
+
+
+
