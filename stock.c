3656b2e9f6efa6231502c242cc525c45c2808017
tanu <24976766+tanusgit@users.noreply.github.com>
2020-07-11 20:26:38 -0700

    stock

--- stock.c
+++ stock.c
@@ -9,20 +9,32 @@
 #include <stdio.h>
 #include <stdio.h>
 int main() {
-    int ar[6]={7,1,5,3,6,4};
+    int ar[7]={100, 180 ,260 ,310, 40, 535, 695};
     int max = 0; 
     int profit = 0;
-    for(int i = 0; i < 5; i++){
+    int index = 0;
+    int index2 = 0;
+    for(int i = 0; i < 6; i++){
         //looking at the back and start at the 1st index and compare those
         //2 elements and if the previous element is lower than the current
         //then subtract and save and it to profit
         int max = ar[i+1];
         if(max > ar[i]){
-            profit =  profit + ar[i+1] - ar[i];
-              
+            profit =  profit + ar[i+1] - ar[i];    
         }
     }
-    printf("profit %d ", profit);
+    for(int i = 0; i < 6; i++){
+        
+        if(ar[i+1] > ar[i]){
+            index = 0;
+           index2 = i+1;
+        }
+        else{
+           index2 = i+1;
+           index = i;
+        }
+    }
+    printf("profit %d %d %d ", profit, index, index2);
     return 0;
 }
 
