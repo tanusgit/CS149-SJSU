0229cfaf0d4fc3b10eedcc8f100e3e3b2040527b
tanu <24976766+tanusgit@users.noreply.github.com>
2020-07-14 19:55:38 -0700

    stock.c

--- stock.c
+++ stock.c
@@ -1,43 +1,64 @@
 //test cases
 //num of days
 //arrays with stock price
-//https://practice.geeksforgeeks.org/problems/stock-buy-and-sell/0git 
+//https://practice.geeksforgeeks.org/problems/stock-buy-and-sell/0git
 
-//7 days                 
+//7 days
 //100 180 260 310 40 535 695
 //output (0 3) (4 6)
 #include <stdio.h>
 #include <stdio.h>
-int main() {
-    int ar[7]={100, 180 ,260 ,310, 40, 535, 695};
-    int max = 0; 
+
+struct keep {
+    int min;
+    int max;
+};
+
+void calculate(int arr[], int num)
+{
+    int min =0;
+    int max = 0;
+     struct keep k[20];
+    for (int i = 0; i < num; i++)
+    {
+        if(arr[i] > arr[i+1]){
+           min = i+1; 
+           max = i;
+           k[i].min = min;
+           k[i].max = max;  
+        }
+        else if(arr[i] < arr[i+1]){
+            min = i;
+            max = i+1;
+            k[i].min = min;
+            k[i].max = max; 
+        } 
+         printf("printing struct %d %d\n", k[i].min, k[i].max); 
+    }
+
+   
+
+}
+
+int main()
+{
+    int ar[7] = {100, 180, 260, 310, 40, 535, 695};
+    calculate(ar, 7);
+    return 0;
+    int max = 0;
     int profit = 0;
     int index = 0;
     int index2 = 0;
-    for(int i = 0; i < 6; i++){
+    for (int i = 0; i < 6; i++)
+    {
         //looking at the back and start at the 1st index and compare those
         //2 elements and if the previous element is lower than the current
         //then subtract and save and it to profit
-        int max = ar[i+1];
-        if(max > ar[i]){
-            profit =  profit + ar[i+1] - ar[i];    
+        int max = ar[i + 1];
+        if (max > ar[i])
+        {
+            profit = profit + ar[i + 1] - ar[i];
         }
     }
-    for(int i = 0; i < 6; i++){
-        
-        if(ar[i+1] > ar[i]){
-            index = 0;
-           index2 = i+1;
-        }
-        else{
-           index2 = i+1;
-           index = i;
-        }
-    }
-    printf("profit %d %d %d ", profit, index, index2);
     return 0;
 }
-
-
-
-
