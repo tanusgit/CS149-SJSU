0b0a80b3d47eef6f1f803b247a27fd4586ae234c
tanu <24976766+tanusgit@users.noreply.github.com>
2020-07-18 19:43:29 -0700

    mem_tracer.c

---
+++ mem_tracer.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(int argc, char *argv[]){
+FILE* ptr = fopen(argv[1],"r" );
+char** array[100][100];
+int i = 0;
+char* buffer;
+    buffer = (char *) malloc (sizeof(char));
+    size_t bufsize = 0;
+    //size_t is always greater than 0
+    int c;
+while ((c = getline(&buffer,&bufsize,ptr))>=0)
+    { 
+        strcpy(&array[i++],buffer);
+        
+    }
+    free(buffer);
+    fclose(ptr);
+     printf("%s\n", &array[0]);
+     printf("%s", &array[1]);
+   /* for(int j =0 ; j < 7; j++){
+       // printf("inside forloop  ");
+        printf("%s", array[j]);
+    }*/
+    
+    return 0;
+
+
+}
+
