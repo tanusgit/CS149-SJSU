7a78125e54354fedad26cdbf2c262ec63972bb79
tanu <24976766+tanusgit@users.noreply.github.com>
2020-07-20 19:28:20 -0700

    mem_tracer

--- mem_tracer.c
+++ mem_tracer.c
@@ -1,32 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int main(int argc, char *argv[]){
-FILE* ptr = fopen(argv[1],"r" );
-char** array[100][100];
-int i = 0;
-char* buffer;
-    buffer = (char *) malloc (sizeof(char));
+typedef struct linkedNode{
+    char* name;
+    int index;
+    linkedNode* next;
+}linkedNode;
+
+linkedNode add(linkedNode head, char* name, int indexnum){
+    linkedNode* head = (linkedNode*)malloc(sizeof(char));
+    if(head == NULL){
+        head = head;
+    }
+    linkedNode* temp = head;
+}
+
+
+int main(int argc, char *argv[])
+{
+    FILE *ptr = fopen(argv[1], "r");
+    char array[100][100];
+    int i = 0;
+    char *buffer;
+
+
+
+
+    buffer = (char *)malloc(sizeof(char));
     size_t bufsize = 0;
     //size_t is always greater than 0
     int c;
-while ((c = getline(&buffer,&bufsize,ptr))>=0)
+    while ((c = getline(&buffer, &bufsize, ptr)) >= 0)
     {
         printf("printing c %d\n", c);
-        strcpy(&array[i++],buffer);
-        
+        strcpy(&array[i++], buffer);
     }
+
+    printf("%s", &array[1]);
     free(buffer);
-    fclose(ptr);
-     printf("%s\n", &array[0]);
-     printf("%s", &array[1]);
-   /* for(int j =0 ; j < 7; j++){
-       // printf("inside forloop  ");
-        printf("%s", array[j]);
-    }*/
-    
+
     return 0;
-
-
 }
-
