0227a826add11286b85a36a792a7d05f86861aeb
tanu <24976766+tanusgit@users.noreply.github.com>
2020-08-10 17:49:51 -0700

    assignment2

--- assig2withforkpipe.c
+++ assig2withforkpipe.c
@@ -4,30 +4,41 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define MSGSIZE 16 
-char* msg1 = "hello, world #1"; 
-char* msg2 = "hello, world #2"; 
-char* msg3 = "hello, world #3"; 
-  
-int main() 
-{ 
-    char inbuf[MSGSIZE]; 
-    int p[2], i; 
-  
-    if (pipe(p) < 0) 
-        exit(1); 
-  
+#define MSGSIZE 16
+char *msg1 = "hello, world #1";
+char *msg2 = "hello, world #2";
+char *msg3 = "hello, world #3";
+
+int main()
+{
+    char inbuf[MSGSIZE];
+    int p[2], i;
+    int f = fork();
+
+    int j = pipe(p);
+
     /* continued */
     /* write pipe */
-  
-    write(p[1], msg1, MSGSIZE); 
-    write(p[1], msg2, MSGSIZE); 
-    write(p[1], msg3, MSGSIZE); 
-  
-    for (i = 0; i < 3; i++) { 
-        /* read pipe */
-        read(p[0], inbuf, MSGSIZE); 
-        printf("% s\n", inbuf); 
-    } 
-    return 0; 
-} 
+    // close(p[0]);
+    if (f == 0)
+    {
+        write(p[1], msg1, MSGSIZE);
+        write(p[1], msg2, MSGSIZE);
+        write(p[1], msg3, MSGSIZE);
+    }
+    // close(p[1]);
+
+    else if (f > 0)
+    {
+        for (i = 0; i < 3; i++)
+        {
+            /* read pipe */
+            // close(p[1]);
+           // wait(NULL);
+            read(p[0], inbuf, MSGSIZE);
+            printf("%s\n", inbuf);
+            // close(p[0]);
+        }
+    }
+    return 0;
+}
