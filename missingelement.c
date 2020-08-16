adccb38ca1efaf03f927bbe025edce17a5a63a45
tanu <24976766+tanusgit@users.noreply.github.com>
2020-08-15 23:08:58 -0700

    missing element in the array

--- missingelement.c
+++ missingelement.c
@@ -4,18 +4,23 @@
 int main()
 {
 
-    int a[] = {1, 2, 3, 4, 5, 6, 7};//7 elements len = 7
+    int a[6] = {1, 2, 3, 4, 5, 6};//7 elements len = 7
     //sort the array
     //1, 2, 3, 4, 6
     //a[4] != 5
     //a[4] != a[4]+1
-    //add all the elements
-    
-    for (int i = 0; i < 6; i++)
+    //add all the elements in the array and then substract from the 
+    //sum of the elements of the entire array
+    int sum = 0;
+    int sum2 = 0;
+    for (int i = 1; i < 8; i++)
     {
-        if (a[i + 1] != a[i] + 1)
-        {
-            printf("%d" , a[i ]+ 1);
-        };
+        sum = sum + i;
     }
+    printf("entire array sum = %d\n", sum);
+    for(int i = 0; i < 6; i++){
+        sum2 = sum2 + a[i];
+    }
+    printf("missing element array sum = %d\n", sum2);
+    printf("missing element in the array = %d\n", (sum - sum2));
 }
