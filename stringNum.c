0035ed8a4485ce5644ed61a514536f3871f012ad
tanu <24976766+tanusgit@users.noreply.github.com>
2020-08-15 20:31:56 -0700

    add integers as strings

---
+++ stringNum.c
@@ -0,0 +1,12 @@
+//given two string as numbers, 
+//add them and return the answer as a string.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    char a = "5";
+    char b = "6";
+    
+    return 0;
+}
